c3aa1fe54808351c23df32c2f45c7dab03f645a6
Eriuden <antoinecarrette@hotmail.fr>
2025-04-13 10:35:13 +0200

    build: kaiju Model 13/04/2025

---
+++ Server/models/kaiju.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const KaijuSchema = new mongoose.Schema({
+  picture: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  threatForHumanity: {
+    type: String,
+    required: true,
+  },
+  threatForEarth: {
+    type: String,
+    required: true,
+  },
+  powers: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 500,
+  },
+});
+
+const KaijuModel = mongoose.model("kaijus", KaijuSchema);
+module.exports = KaijuModel;
